f285f89f577ae40404765372e2920b544957c9d6
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2021-01-20 19:59:47 +0200

    Regexp exec example

---
+++ js/regexp-exec.js
@@ -0,0 +1,10 @@
+const regex1 = RegExp('(f)(Oo*)', 'gi');
+const str1 = 'table football, foosball';
+let array1;
+
+while ((array1 = regex1.exec(str1)) !== null) {
+  //console.log(array1);
+  console.log(`Found ${array1[0]}. Next starts at ${regex1.lastIndex}.`);
+  // expected output: "Found foo. Next starts at 9."
+  // expected output: "Found foo. Next starts at 19."
+}
