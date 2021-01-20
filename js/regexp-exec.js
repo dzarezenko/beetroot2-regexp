609e5fd8ccbdec70e30f06947a765ea0d85a408a
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2021-01-20 20:10:26 +0200

    Regexp test example

--- js/regexp-exec.js
+++ js/regexp-exec.js
@@ -5,6 +5,11 @@ let array1;
 while ((array1 = regex1.exec(str1)) !== null) {
   //console.log(array1);
   console.log(`Found ${array1[0]}. Next starts at ${regex1.lastIndex}.`);
+
+  let g1 = array[1];
+  let g2 = array[2];
+  
+  let [ , g1, g2] = array;
   // expected output: "Found foo. Next starts at 9."
   // expected output: "Found foo. Next starts at 19."
 }
